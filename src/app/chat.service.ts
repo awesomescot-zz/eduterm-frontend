736f579bbf02cf18d0f0e47dd2c0e0f3ddc9d7cb
Scot <scot.spinner@gmail.com>
2017-08-23 22:05:23 -0400

    starting on chat backend calls

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,23 +1,39 @@
 import { Injectable } from '@angular/core';
 import { ChatMessage } from './models/chat-message';
 import { ChatQuestion } from './models/chat-question';
+import { AuthHttp } from 'angular2-jwt';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch'
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class ChatService {
-  messages: ChatMessage[] = [{text: 'hello', message_side: 'right'},
-    {text: 'no more room!!', message_side: 'left'}];
-  questions: ChatQuestion[] = [{text: "what is next?", id: 0, visible: true}, 
-    {text: "Another question!", id: 1, visible: true}];
- 
+  messages: ChatMessage[];// = [{text: 'hello', message_side: 'right'},
+    //{text: 'no more room!!', message_side: 'left'}];
+  questions: ChatQuestion[];// = [{text: "what is next?", id: 0, visible: true},
+  //  {text: "Another question!", id: 1, visible: true}];
+  step: number = 0;
+
   questionClicked(question:ChatQuestion){
     this.messages.push({text:question.text, message_side: 'left'});
-    this.questions = this.questions.map(q => 
+    this.questions = this.questions.map(q =>
       q.id === question.id
         ? {text:q.text, id:q.id, visible: false}
         : q);
   }
 
-  constructor() { 
+  constructor(private authHttp:AuthHttp) {
+  }
+
+  getMessages(){
+    this.authHttp.get("http://localhost:8000/api/chat/{this.step}")
+      .map(res => res.json())
+      .map(
+        data => {
+          this.messages.push(data.chat);
+          this.questions = data.questsions;
+        }
+      ).catch((error:any) => Observable.throw(error));
   }
 
   // sendMessage(text:string, message_side:string) {
